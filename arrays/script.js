04c99ee57187d30b9e7c1bcf72c51e533d3eac55
ssrawat1 <ssr911999@gmail.com>
2025-11-02 18:38:35 +0530

    Update the function name for smallest Num

--- arrays/script.js
+++ arrays/script.js
@@ -2,7 +2,7 @@
 
 let nums = [5, 15, 22, 1, -15, 24];
 
-function getMaxNum(nums) {
+function getMinNum(nums) {
   let maxNum = Number.POSITIVE_INFINITY;
   for (let num of nums) {
     if (maxNum > num) maxNum = num;
@@ -10,6 +10,5 @@ function getMaxNum(nums) {
   return maxNum;
 }
 
-console.log(getMaxNum(nums));
+console.log(getMinNum(nums));
 
- 
