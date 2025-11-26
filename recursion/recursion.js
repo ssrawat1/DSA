37185a0694203b8bcb719263651f0f57bd13ea94
ssrawat1 <ssr911999@gmail.com>
2025-11-26 11:34:22 +0530

    Write the logic to get the factorial using recurison

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -2,12 +2,15 @@
 
  NOTE:   Recursion -> When a function calls itself to solve a problem by breaking it into sub-problems until it reaches a simple base case 
 
+ TC -> Total no. of recursion calls * Work done in each calls
+ SC -> Depth of recursion tree(Height of call stack) * Memory in each call
+
 ***********************************************************************************************************************************************/
 
-/*  
-   Print Numbers from N to 1 and vice vers
-
-*/
+/**************************************************  
+   Print Numbers from N to 1 and vice versa
+ 
+**************************************************/
 
 function printBackward(n = 9) {
   if (n === 1) {
@@ -27,6 +30,20 @@ function printForward(n = 9) {
   console.log(n);
 }
 
- printForward();
- console.log("\n............................\n")
- printBackward()
+printForward();
+console.log('\n............................\n');
+printBackward();
+
+/*******************************************************************
+       Calculate N Factorial using Recursion
+       TC -> O(n)
+       SC -> O(n) (Height of call stack )
+
+ *******************************************************************/
+
+function calculateFactorial(n = 7) {
+  if (n == 1) return 1;
+  return n * calculateFactorial(n - 1);
+}
+
+console.log(calculateFactorial());
