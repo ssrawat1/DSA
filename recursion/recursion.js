7a5658c1b7515a1d91738d5559942c5adb0d2d27
ssrawat1 <ssr911999@gmail.com>
2025-11-27 09:25:20 +0530

    Write the logic to find the target index in the given array

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -83,12 +83,32 @@ console.log(getFibonacciSequence());
 
  *******************************************************************/
 
-function isSorted(arr = [1, 2, 3, 4, 5], index = 0) {
-  if (index === arr.length - 1) return true;
-  if (arr[index] > arr[index + 1]) return false;
-
-  // Check rest of array
-  return isSorted(arr, index + 1);
+function isSorted(arr = [1, 2, 3, 4, 5], n = 5) {
+  if (n === 0 || n === 1) return true;
+  return arr[n - 1] >= arr[n - 2] && isSorted(arr, n - 1);
 }
 
-console.log(isSorted());
+console.log({ isArraySorted: isSorted() });
+
+/*******************************************************************
+          Recursive Binary Search To get the target element index
+                TC -> O(n)
+                SC -> O(n)
+
+ *******************************************************************/
+
+function getTarget(arr = [-1, 0, 3, 5, 9, 12], target = 5, start = 0, end = arr.length - 1) {
+  if (start > end) return -1;
+  let mid = Math.floor((start + end) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] < target) {
+    start = mid + 1;
+    return getTarget(arr, target, start, end);
+  } else if (arr[mid] > target) {
+    end = mid - 1;
+    return getTarget(arr, target, start, end);
+  }
+}
+
+console.log({ targetIndex: getTarget() });
